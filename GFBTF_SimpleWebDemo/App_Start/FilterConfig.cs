f221f3a0b8d6c1683c41be502b2af06f3207fce1
Brian L. Gorman <brian@majorguidancesolutions.com>
2017-09-27 10:59:46 -0500

    Initial Project Commit

---
+++ GFBTF_SimpleWebDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GFBTF_SimpleWebDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GFBTF_SimpleWebDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GFBTF_SimpleWebDemo.Startup))]
+namespace GFBTF_SimpleWebDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
